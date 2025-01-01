3466b58c4433104c248054ef0d70d823165ee7e6
prasa-c <hmc.prasad999@gmail.com>
2025-01-01 17:02:33 +0530

    homepage completed

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,29 +1,55 @@
+"use client";
 import React from 'react'
 import Image from 'next/image'
+import { TypeAnimation } from 'react-type-animation';
+
+
 
 const HeroSection = () => {
     return (
         <section>
-            <div className="grid grid-cols-1 lg:grid-cols-12">
-                <div className="col-span-7 place-self-center">
-                    <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Hello I'm Chathuranga</h1>
-                    <p className="text-[ADB7BE] text-lg lg:text-xl mb-6">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi delectus ipsum at possimus repudiandae nulla odit commodi corporis voluptas. Facere accusantium asperiores quaerat animi vero tempora deleniti est et dolores?
+            <div className="grid grid-cols-1 sm:grid-cols-12">
+                <div className="col-span-7 place-self-center text-center sm:text-left">
+                    <h1 className="text-white sm:text-5xl mb-4 text-4xl lg:text-6xl font-extrabold">
+                        <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>Hello I'm{" "}</span>
+                        <br />
+                        <TypeAnimation
+                            sequence={[
+                                // Same substring at the start will only be typed out once, initially
+                                'Chathuranga',
+                                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                                'Web Developer',
+                                1000,
+                                'Mobile Developer',
+                                1000,
+                                'UI/UX Designer',
+                                1000
+                            ]}
+                            wrapper="span"
+                            speed={30}
+                            
+                            repeat={Infinity}
+                        />
+                    </h1>
+                    <p className="text-[#ADB7BE] text-base sm:text-lg lg:text-xl mb-6">
+                    Designer: Specializing in UI Design and Design Systems.<p>Developer: who writes Clean, Elegant & Efficient Codes. </p> 
                     </p>
                     <div>
-                        <button className='px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black'>Hire Me</button>
-                        <button className='px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3'>Download CV</button>
+                        <button className='px-6 w-full sm:w-fit py-3 rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500  hover:bg-slate-200 text-white'>Hire Me</button>
+                        <button className='px-1 w-full sm:w-fit  py-1 rounded-full bg-transparent bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 mt-3'>
+                            <span className='block bg-black hove:bg-slate-200 rounded-full px-5 py-2'>Download CV</span>
+                        </button>
                     </div>
                 </div>
                 <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-                    <div className="rounded-full bg-[#0f0f0f] w-[250px] h-[250px] ld:w-[400] lg:h-[400] relative">
-                    <Image
-                    src="/images/hero-image.png"
-                    alt="Hero Image"
-                    className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
-                    width={300}
-                    height={300}
-                    />
+                    <div className="rounded-full bg-[#0f0f0f] w-[350px] h-[350px] ld:w-[400] lg:h-[400] relative">
+                        <Image
+                            src="/images/hero-image.png"
+                            alt="Hero Image"
+                            className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
+                            width={300}
+                            height={300}
+                        />
                     </div>
                 </div>
             </div>
